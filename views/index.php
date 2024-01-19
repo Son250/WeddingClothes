87d410581d8770479c7ebec9c158fe9f7843f889
Nguyen Viet Son <anhsongoku123@gmail.com>
2024-01-19 11:01:02 +0700

    Trang Liên hệ

--- views/index.php
+++ views/index.php
@@ -13,6 +13,9 @@ if (isset($_GET['act']) && $_GET['act'] != "") {
         case 'register':
             include 'register.php';
             break;
+        case 'contact':
+            include 'contact.php';
+            break;
         case 'detailProduct':
             include 'detailProduct.php';
             break;
